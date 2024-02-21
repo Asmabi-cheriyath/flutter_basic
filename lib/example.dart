041627888bad69ceaa0a8a51b2aed44e2378f405
ASMABI <asmabicheriyath@gmail.com>
2024-02-21 07:40:40 +0000

    first commit

---
+++ lib/example.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Example extends StatelessWidget {
+  const Example({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("PAGE1"),
+          backgroundColor: Colors.cyan,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: Container(
+            height: 500,
+            width: 500,
+            decoration: BoxDecoration(
+                color: Colors.amber, borderRadius: BorderRadius.circular(30)),
+            child: const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(mainAxisAlignment:MainAxisAlignment.spaceAround,
+                    children: [Text("hello"), Icon(Icons.ac_unit_outlined,size: 50,color: Colors.blueGrey)],
+                  ),
+                  Icon(
+                    Icons.cloud_done,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  Text("hai"),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
